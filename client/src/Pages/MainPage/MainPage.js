688239b1ba374483d0a9c962f44908d75923c321
Thomas Roos <thomas.roos@dawsoncollege.qc.ca>
2024-03-28 01:45:46 -0400

    Setup main page and add some styling

--- client/src/Pages/MainPage/MainPage.js
+++ client/src/Pages/MainPage/MainPage.js
@@ -1,6 +1,9 @@
+// Import CSS
+import './MainPage.css';
+
 export default function MainPage() {
   return(
-    <section id="main-page-component">
+    <section id="main-page-component"  className="custom-border-1">
       <div>Main Page</div>
     </section>
   );
--- client/src/Pages/Navigation/Navigation.js
+++ client/src/Pages/Navigation/Navigation.js
@@ -1,30 +1,47 @@
+// Import dependencies
+import { useNavigate, Outlet } from 'react-router-dom';
+
 // Import CSS
 import './Navigation.css';
 
 export default function Navigation() {
-  return(
-    <section id="navigation-component" className="custom-border-1">
-      {/* Navigation Logo */}
-      <div id="navigation-component-website-container">
-        <div id="navigation-component-website-icon" />
-        <div id="navigation-component-website-title">Arcanum Archives</div>
-      </div>
+  // Establish the navigation
+  const navigate = useNavigate();
 
-      {/* Navigation Headers */}
-      <section id="navigation-component-header-section">
-        <div id="navigation-component-guides-container" className="navigation-component-header-container">
-          <div id="navigation-component-guides-icon" className="navigation-component-icon" />
-          <div id="navigation-component-guides-title" className="navigation-component-title">Guides</div>
+  // Handle Navigation
+  const handleNavigate = (destination) => {
+    // navigate(`/${destination}`);
+  };
+
+  return(
+    <>
+      <section id="navigation-component" className="custom-border-1">
+        {/* Navigation Logo */}
+        <div id="navigation-component-website-container">
+          <div id="navigation-component-website-icon" />
+          <div id="navigation-component-website-title">Arcanum Archives</div>
         </div>
-        <div id="navigation-component-gardening-container" className="navigation-component-header-container">
-          <div id="navigation-component-gardening-icon" className="navigation-component-icon" />
-          <div id="navigation-component-gardening-title" className="navigation-component-title">Gardening</div>
-        </div>
-        <div id="navigation-component-blacksmith-container" className="navigation-component-header-container">
-          <div id="navigation-component-blacksmith-icon" className="navigation-component-icon"/>
-          <div id="navigation-component-blacksmith-title" className="navigation-component-title">Blacksmith</div>
-        </div>
+
+        {/* Navigation Headers */}
+        <section id="navigation-component-header-section">
+          <div id="navigation-component-guides-container" className="navigation-component-header-container">
+            <div id="navigation-component-guides-icon" className="navigation-component-icon" />
+            <div id="navigation-component-guides-title" className="navigation-component-title">Guides</div>
+          </div>
+          <div id="navigation-component-gardening-container" className="navigation-component-header-container">
+            <div id="navigation-component-gardening-icon" className="navigation-component-icon" />
+            <div id="navigation-component-gardening-title" className="navigation-component-title">Gardening</div>
+          </div>
+          <div id="navigation-component-blacksmith-container" className="navigation-component-header-container"
+            onClick={handleNavigate('blacksmith')}>
+            <div id="navigation-component-blacksmith-icon" className="navigation-component-icon"/>
+            <div id="navigation-component-blacksmith-title" className="navigation-component-title">Blacksmith</div>
+          </div>
+        </section>
       </section>
-    </section>
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
