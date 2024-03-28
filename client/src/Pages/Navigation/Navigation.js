f873c1531f72cfd9dd14495097e0c8144984737a
Thomas Roos <thomas.roos@dawsoncollege.qc.ca>
2024-03-28 00:41:40 -0400

    Add custom border, headers, and animations

--- client/src/Pages/Navigation/Navigation.js
+++ client/src/Pages/Navigation/Navigation.js
@@ -1,23 +1,28 @@
-// Import dependencies
-
 // Import CSS
 import './Navigation.css';
 
-// Import Assets
-import WebsiteIcon from '../../Assets/arcanum-icon.png';
-import WebsiteLogo from '../../Assets/ice-logo.png';
-
 export default function Navigation() {
   return(
-    <section id="navigation-component">
+    <section id="navigation-component" className="custom-border-1">
       {/* Navigation Logo */}
-      <img src={WebsiteLogo} alt="Navigation Website Icon" id="navigation-component-website-icon" />
+      <div id="navigation-component-website-container">
+        <div id="navigation-component-website-icon" />
+        <div id="navigation-component-website-title">Arcanum Archives</div>
+      </div>
 
       {/* Navigation Headers */}
-      <section id="navigation-component-header-container">
-        <div id="navigation-component-blacksmith-icon" className="navigation-component-header">
-          <div>Icon #2</div>
-          <div>Blacksmith</div>
+      <section id="navigation-component-header-section">
+        <div id="navigation-component-guides-container" className="navigation-component-header-container">
+          <div id="navigation-component-guides-icon" className="navigation-component-icon" />
+          <div id="navigation-component-guides-title" className="navigation-component-title">Guides</div>
+        </div>
+        <div id="navigation-component-gardening-container" className="navigation-component-header-container">
+          <div id="navigation-component-gardening-icon" className="navigation-component-icon" />
+          <div id="navigation-component-gardening-title" className="navigation-component-title">Gardening</div>
+        </div>
+        <div id="navigation-component-blacksmith-container" className="navigation-component-header-container">
+          <div id="navigation-component-blacksmith-icon" className="navigation-component-icon"/>
+          <div id="navigation-component-blacksmith-title" className="navigation-component-title">Blacksmith</div>
         </div>
       </section>
     </section>
