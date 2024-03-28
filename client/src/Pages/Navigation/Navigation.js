5f1384241fadb64f1ad24dc9ba4548aec95451af
Thomas Roos <thomas.roos@dawsoncollege.qc.ca>
2024-03-27 23:15:32 -0400

    Add icons for each school

--- client/src/Pages/Navigation/Navigation.js
+++ client/src/Pages/Navigation/Navigation.js
@@ -1,7 +1,25 @@
+// Import dependencies
+
+// Import CSS
+import './Navigation.css';
+
+// Import Assets
+import WebsiteIcon from '../../Assets/arcanum-icon.png';
+import WebsiteLogo from '../../Assets/ice-logo.png';
+
 export default function Navigation() {
   return(
     <section id="navigation-component">
-      <div>Hello</div>
+      {/* Navigation Logo */}
+      <img src={WebsiteLogo} alt="Navigation Website Icon" id="navigation-component-website-icon" />
+
+      {/* Navigation Headers */}
+      <section id="navigation-component-header-container">
+        <div id="navigation-component-blacksmith-icon" className="navigation-component-header">
+          <div>Icon #2</div>
+          <div>Blacksmith</div>
+        </div>
+      </section>
     </section>
   );
 }
